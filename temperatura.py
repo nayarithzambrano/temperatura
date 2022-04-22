6ca9dd80155c2df993ec5408d8957e6015ceada1
naya32048 <nayarithzambrano@colegioguanenta.edu.co>
2022-04-22 08:35:35 -0500

    Desarrollo: grados celsius a k y f

---
+++ temperatura.py
@@ -0,0 +1,9 @@
+# input
+c=int(input("Digiti el valor delos grados celcius: "))
+
+# procesing
+k=c+273.15
+f=1.8*c+32
+
+#output
+print(str(c)+ " grados celcius " + " en kelvin es " + str(k) + " y en farenheyt " + str(f))
